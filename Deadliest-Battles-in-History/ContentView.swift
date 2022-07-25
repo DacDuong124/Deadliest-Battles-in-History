ad90171dc89cb102c7ae97a862492bf4e8dcff42
Duong Bui <daniellamb124@gmail.com>
2022-07-25 20:20:45 +0700

    Initial Commit

---
+++ Deadliest-Battles-in-History/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Deadliest-Battles-in-History
+//
+//  Created by Duong, Bui Dang Dac on 25/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Deadliest-Battles-in-History/Deadliest_Battles_in_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deadliest_Battles_in_HistoryApp.swift
+//  Deadliest-Battles-in-History
+//
+//  Created by Duong, Bui Dang Dac on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Deadliest_Battles_in_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
