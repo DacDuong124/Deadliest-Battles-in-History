e594ab57fef19a7baed4613495de4fb023d9daee
DacDuong124 <56530801+DacDuong124@users.noreply.github.com>
2022-08-07 21:24:47 +0700

    Update MapView.swift

--- Deadliest-Battles-in-History/View/MapView.swift
+++ Deadliest-Battles-in-History/View/MapView.swift
@@ -2,6 +2,17 @@
 //  MapView.swift
 //  Deadliest-Battles-in-History
 //
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Bui Dang Dac Duong
+  ID: S3764487
+  Created  date: 27/07/2022 
+  Last modified: 03/08/2022
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 //  Created by Duong, Bui Dang Dac on 02/08/2022.
 //
 
