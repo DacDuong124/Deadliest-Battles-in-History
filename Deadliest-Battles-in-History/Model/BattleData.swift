9647646b0194f7fadbfa0b1c252b656d924a7d61
DacDuong124 <56530801+DacDuong124@users.noreply.github.com>
2022-08-07 21:19:39 +0700

    Update BattleData.swift

--- Deadliest-Battles-in-History/Model/BattleData.swift
+++ Deadliest-Battles-in-History/Model/BattleData.swift
@@ -2,6 +2,17 @@
 //  BattleData.swift
 //  Deadliest-Battles-in-History
 //
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Bui Dang Dac Duong
+  ID: S3764487
+  Created  date: 27/07/2022 
+  Last modified: 06/08/2022
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 //  Created by Duong, Bui Dang Dac on 25/07/2022.
 //
 
