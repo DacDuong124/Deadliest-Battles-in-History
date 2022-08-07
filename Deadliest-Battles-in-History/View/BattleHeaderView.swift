1831bc7984fdbb62485ad3022446378c4605e3cc
DacDuong124 <56530801+DacDuong124@users.noreply.github.com>
2022-08-07 21:15:22 +0700

    Update BattleHeaderView.swift

--- Deadliest-Battles-in-History/View/BattleHeaderView.swift
+++ Deadliest-Battles-in-History/View/BattleHeaderView.swift
@@ -2,6 +2,17 @@
 //  BattleHeaderView.swift
 //  Deadliest-Battles-in-History
 //
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Bui Dang Dac Duong
+  ID: S3764487
+  Created  date: 27/07/2022 
+  Last modified: 30/07/2022
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 //  Created by Duong, Bui Dang Dac on 27/07/2022.
 //
 
